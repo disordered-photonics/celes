c156c3d10c0db50149322686032a0a0f3b062d5c
Amos Egel <amos.egel@kit.edu>
2017-09-13 13:47:47 +0200

    add license headers to fetch_and_patch_gmres and to print_parameters, shift fetch_and_patch_gmres to iterative_solvers folder

--- src/mathematics/iterative_solvers/fetch_and_patch_gmres.m
+++ src/mathematics/iterative_solvers/fetch_and_patch_gmres.m
@@ -28,17 +28,45 @@
 %  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 %  POSSIBILITY OF SUCH DAMAGE.
 
-% ======================================================================
-%> @brief Display the simulation parameters in the console
-% ======================================================================
-function print_parameters(simulation)
+%======================================================================
+%> @brief Copies Matlab's gmres.m function to a temporary folder and adds
+%> status output messages to allow for a live monitoring of the convergence
+%======================================================================
+function fetch_and_patch_gmres()
 
-fprintf(1,'initial field: ');
-if (simulation.input.initialField.beamWidth==0 || simulation.input.initialField.beamWidth==inf)
-    fprintf(1,'plane wave.\n');
-else
-    fprintf(1,'Gaussian beam.\n');
+    out_dir = fullfile(tempdir, 'celes');
+    if ~exist(out_dir, 'dir')
+        mkdir(out_dir);
+    end
+
+    in_dir = fullfile(matlabroot, 'toolbox', 'distcomp', 'array', '+parallel', '+internal', '+flowthrough');
+    copyfile(fullfile(in_dir, 'private', 'iterapp.m'), out_dir);
+    fileattrib(fullfile(out_dir, 'iterapp.m'), '+w');  % make it writable so that it can be overwritten or deleted
+
+    copyfile(fullfile(in_dir, 'itermsg.m'), out_dir);
+    fileattrib(fullfile(out_dir, 'itermsg.m'), '+w');
+
+    path = fullfile(in_dir, 'gmres.m');
+    v = fileread(path);  % read file into vector of chars
+    newline_idxs = find(v == newline);  % find indices of newlines
+
+    first_part_end_idx = newline_idxs(89);
+    second_part_start_idx = newline_idxs(393);
+
+    v_new = [...
+        v(1:44), '_monitor', ...
+        v(45:first_part_end_idx), newline, ...
+        'msg = []; iternum = 0;', newline, ...
+        v(first_part_end_idx:second_part_start_idx), ...
+        '        iternum = iternum + 1; msg = convergence_message(msg, iternum, normr/n2b);', newline, ...
+        v(second_part_start_idx:end)
+    ];
+
+    path = fullfile(out_dir, 'gmres_monitor.m');
+    fd = fopen(path, 'w');
+    fwrite(fd, v_new);
+    fclose(fd);
+
+    addpath(genpath(out_dir));
+
 end
-fprintf(1,'wavelength: %.2f\n',simulation.input.wavelength);
-fprintf(1,'number of particles: %i\n',simulation.input.particles.number);
-
